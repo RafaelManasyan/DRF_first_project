3ee8b98c2ffeb2722e87796294c964b780d271af
rafaelmanasyan <workpageraf@gmail.com>
2025-01-27 18:31:03 +0300

    'project creation'

--- web_sky/views.py
+++ web_sky/views.py
@@ -4,17 +4,29 @@ from rest_framework.viewsets import ModelViewSet
 from web_sky.models import Course, Lesson
 from web_sky.serializers import CourseSerializer, LessonSerializer
 
-
 class CourseViewSet(ModelViewSet):
+    """
+    ViewSet для модели Course.
+    Обеспечивает полный набор операций CRUD (создание, чтение, обновление, удаление) для курсов.
+    """
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-
 class LessonListCreate(ListCreateAPIView):
+    """
+    Представление для получения списка уроков (GET) и создания нового урока (POST).
+    - GET: Возвращает список всех уроков.
+    - POST: Создаёт новый урок с указанными данными.
+    """
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
-
 class LessonRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    """
+    Представление для работы с конкретным уроком.
+    - GET: Возвращает детали урока по его ID.
+    - PUT/PATCH: Обновляет данные урока.
+    - DELETE: Удаляет урок.
+    """
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
